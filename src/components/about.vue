ab1f9d14c8bebb6dcd78ad99d94ec7c9024a78c5
mac <mac.tangji@gmail.com>
2018-07-19 15:26:04 +0800

    简单路由

---
+++ src/components/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <p> about </p>
+</template>
+
+<script>
+
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import home from '@/components/home';
+import about from '@/components/about';
+
 
 Vue.use(Router)
 
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const user = { template: '<div>user: {{ $route.params.names }}</div>' }
+
+
+const routes = [
+  {path:'',component:home},
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar },
+  { path: '/user/:names', component: user },
+  {
+    path: '/home',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: about
+  },
+]
+
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes
 })
